99015371b7e13fd58546eba8835470b14caf94be
nuranavaliyeva <nuranavvaliyeva@gmail.com>
2024-10-10 00:47:20 +0400

    first commit

---
+++ 10oktyabr1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _10oktyabr1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 3, 4,0, 5 };
+            Console.WriteLine(GetMin(arr));
+        }
+        
+         public static int GetMin(int[]arr)
+        {
+           int min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+              
+            }
+            return min;
+        }
+      
+            }
+
+        }
